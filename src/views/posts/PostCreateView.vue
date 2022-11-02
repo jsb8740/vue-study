0b7b3ce1409a38f073b4a0fd0a699a0f1de4496e
jsb <jsb87400@gmail.com>
2022-11-03 02:28:15 +0900

    feat: 수정기능 추가

--- src/views/posts/PostCreateView.vue
+++ src/views/posts/PostCreateView.vue
@@ -4,32 +4,61 @@
     <hr class="my-4" />
     <form @submit.prevent="">
       <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label"> 제목 </label>
-        <input type="text" class="form-control" id="title" />
+        <label for="title" class="form-label"> 제목 </label>
+        <input
+          v-model="form.title"
+          type="text"
+          class="form-control"
+          id="title"
+        />
       </div>
       <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">
-          내요
-        </label>
-        <textarea class="form-control" id="content" rows="3"></textarea>
+        <label for="content" class="form-label"> 내용 </label>
+        <textarea
+          v-model="form.content"
+          class="form-control"
+          id="content"
+          rows="3"
+        ></textarea>
       </div>
 
       <div class="pl-4">
         <button class="btn btn-outline-dark me-2" @click="goListPage">
           목록
         </button>
-        <button class="btn btn-primary">저장</button>
+        <button class="btn btn-primary" @click="save">저장</button>
       </div>
     </form>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { useRouter } from "vue-router";
+import { createPosts } from "@/api/posts";
 
 const router = useRouter();
 const goListPage = () => {
   router.push({ name: "PostList" });
+};
+
+const form = ref({
+  title: "",
+  content: "",
+});
+
+const save = async () => {
+  try {
+    const data = {
+      ...form.value,
+      createdAt: Date.now(),
+    };
+    await createPosts(data);
+
+    router.back();
+  } catch (error) {
+    throw error;
+  }
 };
 </script>
 
