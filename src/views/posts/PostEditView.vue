72eaf2c47772c0eca46c1b5c54b9d1c148b24b90
jsb <jsb87400@gmail.com>
2022-11-03 02:28:38 +0900

    feat: 수정기능 추가

--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -2,23 +2,31 @@
   <div>
     <h2>게시글 수정</h2>
     <hr class="my-4" />
-    <form @submit.prevent="">
+    <form @submit.prevent="edit">
       <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label"> 제목 </label>
-        <input type="text" class="form-control" id="title" />
+        <label for="title" class="form-label"> 제목 </label>
+        <input
+          v-model="form.title"
+          type="text"
+          class="form-control"
+          id="title"
+        />
       </div>
       <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">
-          내용
-        </label>
-        <textarea class="form-control" id="content" rows="3"></textarea>
+        <label for="content" class="form-label"> 내용 </label>
+        <textarea
+          v-model="form.content"
+          class="form-control"
+          id="content"
+          rows="3"
+        ></textarea>
       </div>
 
       <div class="pl-4">
         <button class="btn btn-outline-danger me-2" @click="goDetailPage">
           취소
         </button>
-        <button class="btn btn-primary" @click="goListPage">수정</button>
+        <button class="btn btn-primary">수정</button>
       </div>
     </form>
   </div>
@@ -26,10 +34,12 @@
 
 <script setup lang="ts">
 import { useRoute, useRouter } from "vue-router";
+import { getPostById, updatePosts, type postsInfo } from "@/api/posts";
+import { ref, type Ref } from "vue";
 
 const router = useRouter();
 const route = useRoute();
-const id = route.params.id;
+const id = parseInt(route.params.id as string);
 
 const goDetailPage = () => {
   router.push({ name: "PostDetail", params: { id } });
@@ -39,6 +49,30 @@ const goListPage = () => {
   router.push({
     name: "PostList",
   });
+};
+
+const form: Ref<postsInfo> = ref({
+  title: "",
+  content: "",
+});
+const fetchPost = async () => {
+  const { data } = await getPostById(id);
+  setPost(data);
+};
+const setPost = ({ title, content, createdAt }: postsInfo) => {
+  form.value.title = title;
+  form.value.content = content;
+  form.value.createdAt = createdAt;
+};
+fetchPost();
+
+const edit = async () => {
+  try {
+    await updatePosts(id, { ...form.value });
+    goDetailPage();
+  } catch (error) {
+    throw error;
+  }
 };
 </script>
 
