25fdbec2fc371ad09c90a3ae68a86e499502cef1
jsb <jsb87400@gmail.com>
2022-11-03 02:27:46 +0900

    feat: posts 삭제 json-server연동

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export interface postsInfo {
   id?: number;
   title?: string;
@@ -5,20 +7,28 @@ export interface postsInfo {
   createdAt?: string | Date | number;
 }
 
-const posts = [
-  { id: 1, title: "제목1", content: "내용1", createdAt: "2022-01-21" },
-  { id: 2, title: "제목2", content: "내용2", createdAt: "2022-02-22" },
-  { id: 3, title: "제목3", content: "내용3", createdAt: "2022-03-23" },
-  { id: 4, title: "제목4", content: "내용4", createdAt: "2022-04-24" },
-  { id: 5, title: "제목5", content: "내용5", createdAt: "2022-05-25" },
-  { id: 6, title: "제목6", content: "내용6", createdAt: "2022-06-26" },
-  { id: 7, title: "제목7", content: "내용7", createdAt: "2022-07-27" },
-];
-
-export const getPosts = () => {
-  return posts;
+export const getPosts = (params: {
+  _sort: string;
+  _order: string;
+  _limit: number;
+  _page: number;
+  title_like: string;
+}) => {
+  return axios.get("http://localhost:5000/posts", { params });
 };
 
 export const getPostById = (id: number) => {
-  return posts.find((item) => item.id === id);
+  return axios.get(`http://localhost:5000/posts/${id}`);
+};
+
+export const createPosts = (data: object) => {
+  return axios.post("http://localhost:5000/posts", data);
+};
+
+export const updatePosts = (id: number, data: object) => {
+  return axios.put(`http://localhost:5000/posts/${id}`, data);
+};
+
+export const deletePosts = (id: number) => {
+  return axios.delete(`http://localhost:5000/posts/${id}`);
 };
