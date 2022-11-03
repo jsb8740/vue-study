83e0c65369492753c4714dbfa09c754ef1c4fd72
jsb <jsb87400@gmail.com>
2022-11-04 01:25:46 +0900

    fix: change router props func -> boolean

--- src/router/index.ts
+++ src/router/index.ts
@@ -41,10 +41,10 @@ const routes = [
     path: "/posts/:id",
     name: "PostDetail",
     component: PostDetailView,
-    // props: true,
-    props: (route: any) => ({
-      id: parseInt(route.params.id),
-    }),
+    props: true,
+    // props: (route: any) => ({
+    //   id: parseInt(route.params.id),
+    // }),
   },
   {
     path: "/posts/:id/edit",
